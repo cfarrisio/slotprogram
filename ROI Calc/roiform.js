d15af2a8d28077d8ca64756affed9214fb971a8d
cfarrisio <126063406+cfarrisio@users.noreply.github.com>
2023-04-02 22:20:47 +0200

    Update

--- ROI Calc/roiform.js
+++ ROI Calc/roiform.js
@@ -1,35 +1,64 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {packages: ['corechart']});
 
-function drawChart() {
+document.getElementById("reservation-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
   var adr = parseFloat(document.getElementById("adr").value);
-  var monthly = parseInt(document.getElementById("monthly").value);
+  var monthly = parseFloat(document.getElementById("monthly").value);
   var cost = parseFloat(document.getElementById("cost").value);
-  var direct = parseInt(document.getElementById("direct").value);
-  var channels = parseInt(document.getElementById("channels").value);
-  var roomCount = parseInt(document.getElementById("roomCount").value);
+  var direct = parseFloat(document.getElementById("direct").value);
+  var channels = parseFloat(document.getElementById("channels").value);
+  var occupancy = parseFloat(document.getElementById("occupancy").value);
 
-  var revenue = monthly * adr;
-  var occupancy = (monthly / (30 * roomCount)).toFixed(2);
-  var expenses = cost + (monthly - direct) * (adr * occupancy) + channels * (adr * occupancy);
-  var profit = revenue - expenses;
+  if (isNaN(adr) || isNaN(monthly) || isNaN(cost) || isNaN(direct) || isNaN(channels) || isNaN(occupancy)) {
+    document.getElementById("results").innerHTML = "Please enter valid input for all fields.";
+    document.getElementById("chart").innerHTML = "";
+    document.getElementById("chart2").innerHTML = "";
+  } else {
+    document.getElementById("results").innerHTML = "";
 
-  var data = google.visualization.arrayToDataTable([
-    ['Element', 'Cost', { role: 'style' }],
-    ['Revenue', revenue, 'color: #4CAF50'],
-    ['Expenses', expenses, 'color: #F44336'],
-    ['Profit', profit, 'color: #2196F3']
-  ]);
+    var data = google.visualization.arrayToDataTable([
+      ['Category', 'Value', { role: 'style' }],
+      ['ADR', adr, '#21827c'],
+      ['Monthly Reservations', monthly, '#21827c'],
+      ['Cost of Current System', cost, '#21827c'],
+      ['Number of Direct Reservations', direct, '#21827c'],
+      ['Number of Booking Channels', channels, '#21827c'],
+      ['Occupancy Rate', occupancy, '#21827c']
+    ]);
 
-  var options = {
-    title: 'ROI Chart',
-    width: 600,
-    height: 400,
-    bar: { groupWidth: '75%' },
-    legend: { position: 'none' }
-  };
+    var options = {
+      chartArea: { width: '50%' },
+      hAxis: {
+        title: 'Value',
+        minValue: 0
+      },
+      vAxis: {
+        title: 'Category'
+      },
+      legend: { position: 'none' }
+    };
 
-  var chart = new google.visualization.BarChart(document.getElementById('chart'));
+    var chart = new google.visualization.BarChart(document.getElementById('chart'));
+    chart.draw(data, options);
 
-  chart.draw(data, options);
-}
+    var data2 = google.visualization.arrayToDataTable([
+      ['Opportunity Areas', 'Impact'],
+      ['Marketing and Distribution', 2],
+      ['Pricing', 1],
+      ['Revenue Management', 1],
+      ['Operations', 3],
+      ['Technology', 3]
+    ]);
+
+    var options2 = {
+      chartArea: { width: '50%' },
+      legend: { position: 'none' }
+    };
+
+    var chart2 = new google.visualization.PieChart(document.getElementById('chart2'));
+    chart2.draw(data2, options2);
+
+    document.getElementById("charts").style.display = "block";
+  }
+});
--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import openai
 import os
 import random
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-start_sequence = "\nAI:"
-restart_sequence = "\nHuman: "
-
 app = Flask(__name__, template_folder='templates')
 
-# Define a list of messages
-messages = [{'sender': 'bot', 'text': ''}]
+messages = []
+
 
 # Define a list of welcome messages
 welcome_messages = [
@@ -24,46 +19,48 @@ welcome_messages = [
 # Choose a random welcome message
 welcome_message = random.choice(welcome_messages)
 
+# Initialize the OpenAI API client
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_openai_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=[" Human:", " AI:", "roomraccoon.com"]
+    ).choices[0].text.strip()
+
+    return response
+
+
+def get_response(user_input):
+    prompt = f"I need help with {user_input}."
+    response = get_openai_response(prompt)
+
+    return response
+
+
+@app.route("/")
+def home():
+    global messages, welcome_message
+    welcome_message = random.choice(welcome_messages)
+    return render_template("index.html", welcome_message=welcome_message, messages=messages)
+
 
 @app.route("/send_message", methods=["POST"])
 def send_message():
-    user_input = request.form.get("user_input")
-    messages.append({'sender': 'user', 'text': user_input})
-
-    rr_keywords = ["roomraccoon", "reservation system", "booking engine",
-                   "property management system", "my account", "how do I"]
-    if any(keyword in user_input.lower() for keyword in rr_keywords):
-        if "how do I" in user_input.lower():
-            response = "Sure! Here's how you can " + \
-                user_input.lower().replace("how do I", "") + ": "
-            response += "1. First, go to your Roomraccoon account. "
-            response += "2. Next, navigate to the section where you can manage the settings or options you need to adjust. "
-            response += "3. Finally, update your preferences and save your changes. That's it!"
-        elif "adjust my rates" in user_input.lower():
-            response = "To adjust your rates in your Roomraccoon account, please go to your Account Settings and click on the Rates tab. You can then adjust your rates using the slider below."
-        else:
-            prompt = f"{start_sequence} {user_input} about Roomraccoon{restart_sequence}"
-            response = openai.Completion.create(
-                engine="text-curie-001",
-                prompt=prompt,
-                temperature=0.69,
-                max_tokens=1024,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop=[" Human:", " AI:"]
-            ).choices[0].text.strip()
-
-        # Append the user input and response to the messages list
-        messages.append({'sender': 'user', 'text': user_input})
-        messages.append({'sender': 'bot', 'text': response, 'response': ''})
-
-        # Update the response in the messages list
-        messages[-1]['response'] = response
-
-    return render_template("index.html", response=response, messages=messages)
+    global messages
+    user_input = request.form["user_input"]
+    response = get_response(user_input)
+    messages.append({"sender": "user", "text": user_input})
+    messages.append({"sender": "bot", "text": response})
+    return redirect(url_for("home"))
 
 
-if __name__ == "__main__":
-    print("Starting server...")
+if __name__ == '__main__':
     app.run()
